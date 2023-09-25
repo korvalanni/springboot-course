afc13dd1d5cb4c07248d040f03894ecf0a1fede8
AlibabaRazboinik <korall.sofiya@mail.ru>
2023-09-25 23:10:53 +0500

    start commit

---
+++ src/main/java/ru/urfu/MyFirstAppTest/MyFirstAppTestApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.MyFirstAppTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstAppTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstAppTestApplication.class, args);
+	}
+
+}
